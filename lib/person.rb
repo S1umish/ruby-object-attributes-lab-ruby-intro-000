cc35e2f07e351119521f2927178cb7726077562b
Sunitee Mishra <sunitee.mishra@gmail.com>
2020-08-23 23:01:10 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+  def initialize(name)
+    @name= name
+  end
+  
+  def name
+    @name
+  end
+end
+
+ 
+class Person
+  def initialize (job)
+    @job = job
+  end
+ 
+  def job
+    @job
+  end
+end
